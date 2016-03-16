8b58a195982290638faf1f6628f2d519629f5032
Hamzasgd35 <ahamza2013@namal.edu.pk>
2016-03-16 13:54:18 +0500

    Added files via upload

---
+++ Customer Purchase Record.php
@@ -0,0 +1,193 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Purchase Record</title>
+		<link rel="icon" href="logo.png" type="image/gif" sizes="16x16">
+        <!-- CSS -->
+		<link href="font-awesome-4.3.0/css/font-awesome.min.css" rel="stylesheet">
+		<link href="css/font-awesome.css" rel="stylesheet">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/custom.css" rel="stylesheet" >
+		<link href="css/buttons.css" rel="stylesheet">
+		
+	
+		
+	
+	
+		
+		
+      
+
+    </head>
+
+    <body>
+	<div id="fullscreen_bg" class="fullscreen_bg">
+	
+<div class="header_top"></div>
+
+<div class="menu">
+    <header class="container">
+     <div class="navbar navbar-inner">
+                <div class="container">
+ <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#main-menu">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+                    
+					   
+					   <img src="logo.png" height="100" width="100" >
+
+     </div>
+                    
+				
+					<div class="collapse navbar-collapse pull-right" id="main-menu">
+                        <ul class="nav">
+                            
+
+                             <a href="Customer.html" class="btn btn-success btn-sm "><span class="glyphicon glyphicon-home"></span> Home</a>
+                           <a href="#" class="btn btn-success btn-sm "><span class="glyphicon glyphicon-off"></span> logout</a>
+                        </ul>
+                  <!--	  /.nav-collapse -->
+                </div>
+  
+            </header>
+
+</div><!--menu-->
+
+
+<div class="container">
+
+
+
+        <div class="row centered-form">
+		
+				
+			
+			<div class="col-md-5 col-xs-12 col-md-offset-3" >
+	
+        	<div class="panel panel-default">
+        		<div class="panel-heading">
+				
+				<h3 class="panel-title">Purchase Records </h3>
+			    		
+			 			</div>
+		
+			 			<div class="panel-body">
+						
+						<table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
+        <thead>
+            <tr>
+                <th>Date</th>
+                <th>Item</th>
+                <th>Shop Name</th>
+                <th>Price</th>
+            </tr>
+        </thead>
+       
+        <tbody>
+		
+            
+            
+           
+            <tr>
+                <td>15/1/2016</td>
+                <td>Rice</td>
+                <td>Chakda Mess</td>
+                <td>Rs. 55</td>
+             
+            </tr>
+			 <tr>
+                <td>15/1/2016</td>
+                <td>Rice</td>
+                <td>Chakda Mess</td>
+                <td>Rs. 55</td>
+             
+            </tr>
+<tr>
+                <td>15/1/2016</td>
+                <td>Rice</td>
+                <td>Chakda Mess</td>
+                <td>Rs. 55</td>
+             
+            </tr>
+			 <tr>
+                <td>15/1/2016</td>
+                <td>Rice</td>
+                <td>Chakda Mess</td>
+                <td>Rs. 55</td>
+             
+            </tr>
+<tr>
+                <td>15/1/2016</td>
+                <td>Rice</td>
+                <td>Chakda Mess</td>
+                <td>Rs. 55</td>
+             
+            </tr>
+			 
+                 
+        </tbody>
+		
+		
+    </table>
+						
+						
+						
+						
+				
+						
+						
+						
+			    	
+	    		</div>
+    		</div>
+			
+			
+			
+    	</div>
+    </div>
+	
+	
+	
+	
+	
+	</div>
+	</div>
+    </div>
+	</div>
+    
+
+
+</body>
+	
+
+ 
+                
+ 
+
+        <!-- Javascript -->
+        
+        <script src="js/jquery-1.10.2.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="http://www.clubdesign.at/floatlabels.js"></script>
+		
+        <!--[if lt IE 10]>
+            <script src="assets/js/placeholder.js"></script>
+        <![endif]-->
+
+    </body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<?php    ?>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>E-Wallet</title>
+		<link rel="icon" href="logo.png" type="image/gif" sizes="16x16">
+        <!-- CSS -->
+		<link href="font-awesome-4.3.0/css/font-awesome.min.css" rel="stylesheet">
+		<link href="css/font-awesome.css" rel="stylesheet">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/custom.css" rel="stylesheet" >
+		<link href="css/buttons.css" rel="stylesheet">
+		
+	
+		
+		
+      
+
+    </head>
+
+    <body>
+	<div id="fullscreen_bg" class="fullscreen_bg">
+	
+<div class="header_top"></div>
+
+<div class="menu">
+    <header class="container">
+     <div class="navbar navbar-inner">
+                <div class="container">
+ <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#main-menu">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+                    
+					   
+					   <img src="logo.png" height="100" width="100" >
+
+     </div>
+                    
+				<!--	
+					<div class="collapse navbar-collapse pull-right" id="main-menu">
+                        <ul class="nav">
+                            
+
+                            <li class="fadeInDown animated d3"><a href="#Iletisim" menuid="11" followlink="true">Dislaimer</a></li>
+                            
+                             <li class="fadeInDown animated d2"><a href="#" menuid="1" followlink="true">Blog</a></li>
+                             
+                             <li class="fadeInDown animated d2"><a href="#" menuid="1" followlink="true">Contact Us</a></li>
+
+                        </ul>
+                    /.nav-collapse -->
+                </div>
+  
+            </header>
+
+</div><!--menu-->
+
+
+<div class="container">
+        <div class="row centered-form">
+		
+		
+        <div class="col-md-4 col-xs-12 ">
+	
+        	<div class="panel panel-default">
+        		<div class="panel-heading">
+			    		<h3 class="panel-title">Login </h3>
+			 			</div>
+			 			<div class="panel-body">
+			    		<form role="form">
+			    			<div class="row">
+			    				<div class="col-xs-12 col-md-12">
+			    					<div class="form-group">
+			                <input type="text" name="Username" id="username" class="form-control input-sm floatlabel" placeholder="Username">
+			    					</div>
+			    				</div>
+			    				
+			    			</div>
+
+			    			
+
+			    			<div class="row">
+			    				<div class="col-xs-12 col-md-12">
+			    					<div class="form-group">
+			    						<input type="password" name="password" id="password" class="form-control input-sm" placeholder="Password">
+			    					</div>
+			    				</div>
+			  
+			    			</div>
+			    			
+			    			<a href="#" class="btn btn-success gradient pull-right"><span class="glyphicon glyphicon-log-in"></span> Proceed</a>
+			    		
+			    		</form>
+			    	</div>
+	    		</div>
+    		</div>
+			
+			
+			
+			<div class="col-md-4 " >
+</div>			
+			
+			<div class="col-md-4 col-xs-12" >
+	
+        	<div class="panel panel-default">
+        		<div class="panel-heading">
+			    		<h3 class="panel-title">Signup </h3>
+			 			</div>
+			 			<div class="panel-body">
+			    		<form role="form">
+			    			<div class="row">
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+			                <input type="text" name="first_name" id="first_name" class="form-control input-sm floatlabel" placeholder="First Name">
+			    					</div>
+			    				</div>
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+			    						<input type="text" name="last_name" id="last_name" class="form-control input-sm" placeholder="Last Name">
+			    					</div>
+			    				</div>
+			    			</div>
+
+			    			<div class="form-group">
+			    				<input type="email" name="email" id="email" class="form-control input-sm" placeholder="Email Address">
+			    			</div>
+							<div class="form-group">
+			    				<input type="username" name="username" id="username" class="form-control input-sm" placeholder="Username">
+			    			</div>
+
+			    			<div class="row">
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+			    						<input type="password" name="password" id="password" class="form-control input-sm" placeholder="Password">
+			    					</div>
+			    				</div>
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+			    						<input type="password" name="password_confirmation" id="password_confirmation" class="form-control input-sm" placeholder="Confirm Password">
+			    					</div>
+			    				</div>
+			    			</div>
+							<div class = "row">
+							<img src="download.png" height="50" width="50" class="img-responsive center-block" >
+							</br>
+							</div>
+			    			
+			    			<a href="#" class="btn btn-success gradient pull-right"><span class="glyphicon glyphicon-log-in"></span> Proceed</a>
+			    		
+			    		</form>
+			    	</div>
+	    		</div>
+    		</div>
+			
+			
+			
+    	</div>
+    </div>
+
+
+</body>
+	
+
+ 
+                
+ 
+
+        <!-- Javascript -->
+        
+        <script src="js/jquery-1.10.2.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="http://www.clubdesign.at/floatlabels.js"></script>
+        
+        <!--[if lt IE 10]>
+            <script src="assets/js/placeholder.js"></script>
+        <![endif]-->
+
+    </body>
+
+</html>
